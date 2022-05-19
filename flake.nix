d2e800ae1cdd86c64aced1358d7a9d6c784e812c
NomisIV <simon@nomisiv.com>
2022-05-19 21:35:35 +0200

    Add alejandra (and format code)

--- flake.nix
+++ flake.nix
@@ -1,20 +1,28 @@
 {
   description = "A flake for generating the content of my website";
 
-  outputs = { self, nixpkgs, diosevka, servera }:
-    let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      lib = import ./lib.nix { inherit pkgs; };
-    in
-    with lib;
-    {
-      nixosModule = { lib, config, ... }: let
+  outputs = {
+    self,
+    nixpkgs,
+    diosevka,
+    servera,
+  }: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    lib = import ./lib.nix {inherit pkgs;};
+  in
+    with lib; {
+      nixosModule = {
+        lib,
+        config,
+        ...
+      }: let
         cfg = config.services.nomisiv-website;
       in {
         options.services.nomisiv-website = with lib; {
           enable = mkEnableOption "NomisIV's website";
 
-          openFirewall = mkEnableOption
+          openFirewall =
+            mkEnableOption
             "open the port for the website in the firewall";
 
           port = mkOption {
@@ -39,13 +47,12 @@
         config = lib.mkIf cfg.enable {
           systemd.services.nomisiv-website = {
             description = "NomisIV website web server";
-            after = [ "network.target" ];
-            requires = [ "network.target" ];
-            wantedBy = [ "multi-user.target" ];
+            after = ["network.target"];
+            requires = ["network.target"];
+            wantedBy = ["multi-user.target"];
 
             serviceConfig = {
-              ExecStart =
-                "${servera.packages.x86_64-linux.default}/bin/servera \
+              ExecStart = "${servera.packages.x86_64-linux.default}/bin/servera \
                 ${toString cfg.port} ${self.packages.x86_64-linux.default}";
               User = cfg.user;
               Group = cfg.group;
@@ -54,7 +61,7 @@
 
           # Open firewall
           networking.firewall = lib.mkIf cfg.openFirewall {
-            allowedTCPPorts = [ cfg.port ];
+            allowedTCPPorts = [cfg.port];
           };
 
           # Add user and group
@@ -77,23 +84,29 @@
           age = pkgs.lib.strings.removeSuffix "\n" (builtins.readFile (
             pkgs.runCommand
             "age"
-            { buildInputs = with pkgs; [ dateutils ]; }
+            {buildInputs = with pkgs; [dateutils];}
             "datediff 20020219101400 ${self.lastModifiedDate} \\\
             -i %Y%m%d%H%M%S -f %Y > $out"
           ));
-          email = pkgs.lib.strings.removeSuffix "\n"
+          email =
+            pkgs.lib.strings.removeSuffix "\n"
             (builtins.readFile ./email.html);
         };
 
-        customHtmlTemplate = { title, body, extraSubs ? { } }:
+        customHtmlTemplate = {
+          title,
+          body,
+          extraSubs ? {},
+        }:
           htmlTemplate (let
-            description = pkgs.lib.strings.escapeXML
+            description =
+              pkgs.lib.strings.escapeXML
               (builtins.readFile ./description.txt);
           in {
             inherit title description;
             body = mdToHtml (substitute (substitutions // extraSubs) body);
             favicon = "/favicon.ico";
-            stylesheets = [ "/style.css" ];
+            stylesheets = ["/style.css"];
             themeColor = "#d79921";
             openGraph = {
               inherit description;
@@ -108,7 +121,7 @@
           (substitute substitutions file)
           (scssToCss (
             substitute
-            { diosevka = diosevka.packages.x86_64-linux.ttf; }
+            {diosevka = diosevka.packages.x86_64-linux.ttf;}
             ./src/pdf.scss
           ));
       in {
@@ -165,62 +178,91 @@
         assets = {
           "nomisiv.svg" = ./src/assets/nomisiv.svg;
           "card.png" = ./src/assets/card.png;
-          fonts = diosevka.packages.x86_64-linux.woff2
-            + "/share/fonts/diosevka/woff2" ;
+          fonts =
+            diosevka.packages.x86_64-linux.woff2
+            + "/share/fonts/diosevka/woff2";
           memes = ./src/assets/memes;
         };
 
         blog = let
           blogPagesList = [
-            { date = "2022-02-02"; title = "How I Rebuilt This Website"; }
-            { date = "2021-10-08"; title = "Android Sucks";              }
-            { date = "2021-09-18"; title = "My Battlestation Part 2";    }
-            { date = "2021-08-23"; title = "My Battlestation Part 1";    }
-            { date = "2021-06-05"; title = "How I Built This Website";   }
+            {
+              date = "2022-02-02";
+              title = "How I Rebuilt This Website";
+            }
+            {
+              date = "2021-10-08";
+              title = "Android Sucks";
+            }
+            {
+              date = "2021-09-18";
+              title = "My Battlestation Part 2";
+            }
+            {
+              date = "2021-08-23";
+              title = "My Battlestation Part 1";
+            }
+            {
+              date = "2021-06-05";
+              title = "How I Built This Website";
+            }
           ];
 
           linkOf = page: "${page.date}-${camelCaseToKebabCase page.title}";
-        in {
-          "index.html" = customHtmlTemplate {
-            title = "No one asked";
-            body = ./src/blog/index.md;
-            extraSubs = {
-              blog = builtins.concatStringsSep "\n" (
-                map
-                (
-                  value:
-                  "- [${value.date} ${value.title}](/blog/${linkOf value})"
-                )
-                blogPagesList
-              );
+        in
+          {
+            "index.html" = customHtmlTemplate {
+              title = "No one asked";
+              body = ./src/blog/index.md;
+              extraSubs = {
+                blog = builtins.concatStringsSep "\n" (
+                  map
+                  (
+                    value: "- [${value.date} ${value.title}](/blog/${linkOf value})"
+                  )
+                  blogPagesList
+                );
+              };
             };
-          };
-        } // builtins.listToAttrs (map (blogPost: {
-          name = linkOf blogPost + ".html";
-          value = customHtmlTemplate {
-            title = blogPost.title;
-            body = ./src/blog + ("/" + linkOf blogPost) + ".md";
-          };
-        }) blogPagesList);
+          }
+          // builtins.listToAttrs (map (blogPost: {
+              name = linkOf blogPost + ".html";
+              value = customHtmlTemplate {
+                title = blogPost.title;
+                body = ./src/blog + ("/" + linkOf blogPost) + ".md";
+              };
+            })
+            blogPagesList);
 
         errors = let
           errorPagesList = [
-            { code = 404; message = "Not Found";             }
-            { code = 500; message = "Internal server Error"; }
+            {
+              code = 404;
+              message = "Not Found";
+            }
+            {
+              code = 500;
+              message = "Internal server Error";
+            }
           ];
-        in builtins.listToAttrs (map (errorPage: {
-          name = "${toString errorPage.code}.html";
-          value = htmlTemplate {
-            title = "${toString errorPage.code} ${errorPage.message}";
-            body = mdToHtml (./src/errors
-              + ("/" + toString errorPage.code)
-              + ".md"
-            );
-            favicon = "/favicon.ico";
-            stylesheets = [ "/style.css" ];
-            themeColor = "#cc241d";
-          };
-        }) errorPagesList);
-    });
-  };
+        in
+          builtins.listToAttrs (map (errorPage: {
+              name = "${toString errorPage.code}.html";
+              value = htmlTemplate {
+                title = "${toString errorPage.code} ${errorPage.message}";
+                body = mdToHtml (
+                  ./src/errors
+                  + ("/" + toString errorPage.code)
+                  + ".md"
+                );
+                favicon = "/favicon.ico";
+                stylesheets = ["/style.css"];
+                themeColor = "#cc241d";
+              };
+            })
+            errorPagesList);
+      });
+
+      formatter.x86_64-linux = pkgs.alejandra;
+    };
 }
--- lib.nix
+++ lib.nix
@@ -1,29 +1,27 @@
-{ pkgs }:
+{pkgs}:
 with pkgs;
-with lib;
-rec {
+with lib; rec {
   mkSite = url: pages:
-  runCommand url {} (let
-    pageNamesFlat = attrsets.collect builtins.isString (
-      attrsets.mapAttrsRecursive
-      (path: value: strings.concatStringsSep "/" path)
-      pages
-    );
+    runCommand url {} (let
+      pageNamesFlat = attrsets.collect builtins.isString (
+        attrsets.mapAttrsRecursive
+        (path: value: strings.concatStringsSep "/" path)
+        pages
+      );
 
-    pageMap = attrsets.genAttrs pageNamesFlat ( pageUrl:
-      attrsets.getAttrFromPath (strings.splitString "/" pageUrl) pages
-    );
+      pageMap = attrsets.genAttrs pageNamesFlat (
+        pageUrl:
+          attrsets.getAttrFromPath (strings.splitString "/" pageUrl) pages
+      );
 
-    genPageList = attrsets.mapAttrsToList (name: value:
-    ''
-      mkdir -p $(dirname $out/${name})
-      ln -s ${value} $out/${name}
-    '') pageMap;
-  in concatStringsSep "\n" (genPageList));
+      genPageList = attrsets.mapAttrsToList (name: value: ''
+        mkdir -p $(dirname $out/${name})
+        ln -s ${value} $out/${name}
+      '') pageMap;
+    in
+      concatStringsSep "\n" genPageList);
 
-  substitute =
-    substitutions:
-    inFile:
+  substitute = substitutions: inFile:
     runCommand
     ((fileName inFile) + "-sub")
     {}
@@ -36,8 +34,7 @@ rec {
       subsList = mapAttrsToList mapFn substitutions;
 
       subsStr = concatStringsSep " " subsList;
-    in
-    ''
+    in ''
       substitute ${inFile} $out ${subsStr}
     '');
 
@@ -49,134 +46,127 @@ rec {
     description ? null,
     themeColor ? null,
     openGraph ? null,
-  }: with strings; let
-    faviconStr =
-      optionalString
-      (favicon != null)
-      "<link rel=\"icon\" type=\"image/x-icon\" href=\"${favicon}\">";
+  }:
+    with strings; let
+      faviconStr =
+        optionalString
+        (favicon != null)
+        "<link rel=\"icon\" type=\"image/x-icon\" href=\"${favicon}\">";
 
-    stylesheetsStr =
-      pkgs.lib.strings.optionalString
-      (stylesheets != null)
-      (
-        concatMapStringsSep
-        "\n"
-        (stylesheet: "<link rel=\"stylesheet\" href=\"${stylesheet}\">")
-        stylesheets
-      );
+      stylesheetsStr =
+        pkgs.lib.strings.optionalString
+        (stylesheets != null)
+        (
+          concatMapStringsSep
+          "\n"
+          (stylesheet: "<link rel=\"stylesheet\" href=\"${stylesheet}\">")
+          stylesheets
+        );
 
-    descriptionStr =
-      optionalString
-      (description != null)
-      "<meta name=\"description\"  content=\"${description}\">";
+      descriptionStr =
+        optionalString
+        (description != null)
+        "<meta name=\"description\"  content=\"${description}\">";
 
-    themeColorStr =
-      optionalString
-      (themeColor != null)
-      "<meta name=\"theme-color\"  content=\"${themeColor}\">";
+      themeColorStr =
+        optionalString
+        (themeColor != null)
+        "<meta name=\"theme-color\"  content=\"${themeColor}\">";
 
-    openGraphStr =
-      optionalString
-      (openGraph != null)
-      ''
-        <meta property="og:url" content="https://${openGraph.url}/">
-        <meta property="og:type" content="website">
-        <meta property="og:title" content="${openGraph.title}">
-        <meta property="og:description" content="${openGraph.description}">
-        <meta property="og:image" content="${openGraph.image}">
-        <meta property="og:image:type" content="image/png">
-        <meta property="og:image:width" content="1200">
-        <meta property="og:image:height" content="630">
+      openGraphStr =
+        optionalString
+        (openGraph != null)
+        ''
+          <meta property="og:url" content="https://${openGraph.url}/">
+          <meta property="og:type" content="website">
+          <meta property="og:title" content="${openGraph.title}">
+          <meta property="og:description" content="${openGraph.description}">
+          <meta property="og:image" content="${openGraph.image}">
+          <meta property="og:image:type" content="image/png">
+          <meta property="og:image:width" content="1200">
+          <meta property="og:image:height" content="630">
 
-        <meta name="twitter:card" content="summary_large_image">
-        <meta name="twitter:domain" content="${openGraph.url}">
-        <meta name="twitter:url" content="https://${openGraph.url}/">
-        <meta name="twitter:title" content="${openGraph.title}">
-        <meta name="twitter:description" content="${openGraph.description}">
-        <meta name="twitter:image" content="${openGraph.image}">
+          <meta name="twitter:card" content="summary_large_image">
+          <meta name="twitter:domain" content="${openGraph.url}">
+          <meta name="twitter:url" content="https://${openGraph.url}/">
+          <meta name="twitter:title" content="${openGraph.title}">
+          <meta name="twitter:description" content="${openGraph.description}">
+          <meta name="twitter:image" content="${openGraph.image}">
+        '';
+      html = ''
+        <!DOCTYPE html>
+        <html lang="en">
+          <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+            ${faviconStr}
+            ${stylesheetsStr}
+            ${descriptionStr}
+            ${themeColorStr}
+            ${openGraphStr}
+
+            <title>${title}</title>
+          </head>
+          <body>
+          ${builtins.readFile body}
+          </body>
+        </html>
       '';
-    html = ''
-      <!DOCTYPE html>
-      <html lang="en">
-        <head>
-          <meta charset="utf-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-          ${faviconStr}
-          ${stylesheetsStr}
-          ${descriptionStr}
-          ${themeColorStr}
-          ${openGraphStr}
-
-          <title>${title}</title>
-        </head>
-        <body>
-        ${builtins.readFile body}
-        </body>
-      </html>
-    '';
-  in builtins.toString (writeText (fileName body) html);
+    in
+      builtins.toString (writeText (fileName body) html);
 
   # remove the /nix/store/<hash>- part of a file path
   fileName = filePath:
     builtins.substring 44 (builtins.stringLength filePath) filePath;
 
-  replaceExt =
-    filePath:
-    newExt:
-    let
-      fileNameNoExt = strings.concatStringsSep "." (
-        lists.init
-        (strings.splitString "." (fileName filePath))
-      );
-    in
+  replaceExt = filePath: newExt: let
+    fileNameNoExt = strings.concatStringsSep "." (
+      lists.init
+      (strings.splitString "." (fileName filePath))
+    );
+  in
     fileNameNoExt + newExt;
 
-  mdToHtml =
-    md:
+  mdToHtml = md:
     builtins.toString (
       runCommand
       (replaceExt md ".html")
-      { buildInputs = [ pandoc ]; }
+      {buildInputs = [pandoc];}
       "pandoc \\\
       --from markdown+autolink_bare_uris-implicit_figures \\\
       --output $out ${md}"
     );
 
-  scssToCss =
-    scss:
+  scssToCss = scss:
     builtins.toString (
       runCommand
       (replaceExt scss ".css")
-      { buildInputs = [ sassc ]; }
+      {buildInputs = [sassc];}
       "sassc ${scss} $out"
     );
 
-  svgToIco =
-    svg:
+  svgToIco = svg:
     builtins.toString (
       runCommand
       (replaceExt svg ".ico")
-      { buildInputs = [ imagemagick ]; }
+      {buildInputs = [imagemagick];}
       "convert -resize 16x16 -background transparent ${svg} $out"
     );
 
-  svgToPng =
-    svg:
+  svgToPng = svg:
     builtins.toString (
       runCommand
       (replaceExt svg ".png")
-      { buildInputs = [ imagemagick ]; }
+      {buildInputs = [imagemagick];}
       "convert ${svg} $out"
     );
 
-  mdToPdf =
-    md:
-    css:
+  mdToPdf = md: css:
     builtins.toString (
       runCommand
       (replaceExt md ".pdf")
-      { buildInputs = [ pandoc wkhtmltopdf ]; }
+      {buildInputs = [pandoc wkhtmltopdf];}
       "pandoc ${md} \\\
       --output $out \\\
       --css ${css} \\\
@@ -187,7 +177,6 @@ rec {
       --variable margin-right=30"
     );
 
-  camelCaseToKebabCase =
-    string:
+  camelCaseToKebabCase = string:
     concatStringsSep "-" (map toLower (splitString " " string));
 }
